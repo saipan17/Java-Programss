76cdf182e0d961727e8c9441f4757eb8d5d8bd18
saipan17 <msaipanmulook@gmail.com>
2024-02-26 21:55:56 +0530

    Fibonacci Series in java

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+public class Fibonacci
+{
+	public static void main(String[] args) {
+
+        int a = 0;
+        int b = 1;
+        
+        for(int i=2; i<=10; i++){
+            
+            int c = a+b;
+            
+            System.out.println(a);
+            
+            a = b;
+            b = c;
+            
+            
+        }
+
+	}
+}
