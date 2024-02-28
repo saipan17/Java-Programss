cec1eaf2f1bc5ade41bb285ad9a25559748e5c11
saipan17 <msaipanmulook@gmail.com>
2024-02-29 00:33:41 +0530

    print the elements of array in reverse order

---
+++ ReverseArray.java
@@ -0,0 +1,11 @@
+public class ReverseArray
+{
+	public static void main(String[] args) {
+		
+		int arr[] = {4,6,7,8,12,34,1,9};
+		
+		for(int i = arr.length-1; i>=0; i--){
+		    
+		      System.out.println(arr[i]);		}
+	}
+}
