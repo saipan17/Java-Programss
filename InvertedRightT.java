f1925accba27fa34878f01700f088b52a1fe4b79
saipan17 <msaipanmulook@gmail.com>
2024-03-07 08:50:16 +0530

    Inverted Right Trianlge pattern in java

---
+++ InvertedRightT.java
@@ -0,0 +1,14 @@
+public class InvertedRightT
+{
+	public static void main(String[] args) {
+	
+	     for(int i = 1; i<=8; i++){
+	         
+	         for(int j = 7; j>i; j--){
+	             
+	             System.out.print("*");
+	         }
+	         System.out.println("");
+	     }
+	}
+}
