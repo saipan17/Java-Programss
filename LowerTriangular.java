ab1712ce323391354e89ffa52286b1a31834ea69
saipan17 <msaipanmulook@gmail.com>
2024-03-21 14:45:53 +0530

    Java program to print the lower triangular pattern

---
+++ LowerTriangular.java
@@ -0,0 +1,32 @@
+public class LowerTriangular
+{
+	public static void main(String[] args) {
+	
+	            int a[][] = {       
+                        {1, 2, 3},    
+                        {8, 6, 4},    
+                        {4, 5, 6}    
+                    };
+                    
+              int rows = a.length;
+              int cols = a.length;
+              
+              
+              for(int i = 0; i<rows; i++){
+                  
+                  for(int j = 0; j<cols; j++){
+                      
+                      if(j>i){
+                          
+                          System.out.print(0);
+                          
+                      }else{
+                          
+                          System.out.print(a[i][j] + " ");
+                      }
+                  }
+                  
+                  System.out.println("");
+              }
+	}
+}
